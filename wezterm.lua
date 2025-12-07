51116a3c998318512649494297c67c99877670cd
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-12-07 13:27:27 -0500

    fix: format wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -1,12 +1,12 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- Remove window padding.
+-- Minimize window padding.
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 1,
+	right = 1,
+	top = 1,
+	bottom = 1,
 }
 
 -- Configure tab bar.
@@ -14,10 +14,10 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 
 -- Configure font.
-config.font = wezterm.font {
-  family = "ZedMono Nerd Font",
-  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-}
-config.font_size = 16
+config.font = wezterm.font({
+	family = "ZedMono Nerd Font",
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+})
+config.font_size = 15
 
 return config
