8a98019297a5537100cc33cc2568a17416655873
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-12-07 13:27:09 -0500

    fix: remove awk call in rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 [[ $# -gt 0 ]] && cd "$HOME/nix" && nh os switch . && {
-  git add --all
-  git commit --message "$* [$(
-    sudo nix-env --list-generations --profile /nix/var/nix/profiles/system |
-      grep current |
-      awk '{$1=$1};1' |
-      cut -d' ' -f1
-  )]"
-  git push --quiet
+    git add --all
+    git commit --message "$* [$(
+        sudo nix-env --list-generations --profile /nix/var/nix/profiles/system |
+            grep current |
+            cut -d' ' -f3
+    )]"
+    git push --quiet
 }
