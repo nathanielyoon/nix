f7c39d7260273c3746732ddec071d7c003a0b668
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-12-07 12:01:15 -0500

    fix: auto-format kdl [38]

--- home.nix
+++ home.nix
@@ -73,53 +73,54 @@ in
       display-inlay-hints = false;
     };
     settings.keys =
-    let
-      normal = {
-        "C-v" = "signature_help";
-        "Y" = "yank_joined";
-        "a" = [
-          "append_mode"
-          "collapse_selection"
-        ];
-        "i" = [
-          "insert_mode"
-          "collapse_selection"
-        ];
-        "~" = "switch_to_lowercase";
-        "`" = "switch_case";
-        "x" = "extend_line";
-        "C-h" = ":toggle-option lsp.display-inlay-hints";
+      let
+        normal = {
+          "C-v" = "signature_help";
+          "Y" = "yank_joined";
+          "a" = [
+            "append_mode"
+            "collapse_selection"
+          ];
+          "i" = [
+            "insert_mode"
+            "collapse_selection"
+          ];
+          "~" = "switch_to_lowercase";
+          "`" = "switch_case";
+          "x" = "extend_line";
+          "C-h" = ":toggle-option lsp.display-inlay-hints";
+        };
+      in
+      {
+        normal = normal // {
+          "H" = [
+            "select_mode"
+            "ensure_selections_forward"
+            "flip_selections"
+            "goto_first_nonwhitespace"
+          ];
+          "L" = [
+            "select_mode"
+            "ensure_selections_forward"
+            "goto_line_end"
+          ];
+        };
+        select = normal // {
+          "H" = [
+            "ensure_selections_forward"
+            "flip_selections"
+            "goto_first_nonwhitespace"
+          ];
+          "L" = [
+            "ensure_selections_forward"
+            "goto_line_end"
+          ];
+        };
+        insert = {
+          "C-v" = "signature_help";
+          "C-h" = ":toggle-option lsp.display-inlay-hints";
+        };
       };
-    in {
-      normal = normal // {
-        "H" = [
-          "select_mode"
-          "ensure_selections_forward"
-          "flip_selections"
-          "goto_first_nonwhitespace"
-        ];
-        "L" = [
-          "select_mode"
-          "ensure_selections_forward"
-          "goto_line_end"
-        ];
-      };
-      select = normal // {
-        "H" = [
-          "ensure_selections_forward"
-          "flip_selections"
-          "goto_first_nonwhitespace"
-        ];
-        "L" = [
-          "ensure_selections_forward"
-          "goto_line_end"
-        ];
-      };
-      insert = {
-        "C-v" = "signature_help";
-        "C-h" = ":toggle-option lsp.display-inlay-hints";
-      };
-    };
     languages.language-server = {
       deno-lsp = {
         command = "deno";
@@ -178,152 +179,156 @@ in
       };
     };
     languages.language =
-    let
-      deno-fmt = extension: {
-        command = "deno";
-        args = [
-          "fmt"
-          "-"
-          "--ext"
-          extension
-        ];
-      };
-    in
-    [
-      {
-        name = "nix";
-        formatter.command = "nixfmt";
-        language-servers = [
-          "nixd"
-          "nil"
-        ];
-        auto-format = true;
-      }
-      {
-        name = "bash";
-        indent = {
-          tab-width = 4;
-          unit = "    ";
-        };
-        formatter = {
-          command = "shfmt";
-          args = [
-            "-i"
-            "4"
-          ];
-        };
-        auto-format = true;
-      }
-      {
-        name = "typst";
-        language-servers = [ "tinymist" ];
-        formatter = {
-          command = "typstyle";
-          args = [ "--wrap-text" ];
-        };
-        auto-format = true;
-      }
-      {
-        name = "toml";
-        formatter = {
-          command = "taplo";
+      let
+        deno-fmt = extension: {
+          command = "deno";
           args = [
             "fmt"
             "-"
+            "--ext"
+            extension
           ];
         };
-        auto-format = true;
-      }
-      {
-        name = "html";
-        language-servers = [ "superhtml" ];
-        formatter = {
-          command = "superhtml";
-          args = [
-            "fmt"
-            "--stdin"
+      in
+      [
+        {
+          name = "nix";
+          formatter.command = "nixfmt";
+          language-servers = [
+            "nixd"
+            "nil"
           ];
-        };
-        auto-format = true;
-      }
-      {
-        name = "typescript";
-        shebangs = [ "deno" ];
-        roots = [
-          "deno.json"
-          "deno.jsonc"
-          "package.json"
-          "tsconfig.json"
-        ];
-        file-types = [
-          "ts"
-          "mts"
-          "cts"
-        ];
-        language-servers = [ "deno-lsp" ];
-        formatter = deno-fmt "ts";
-        auto-format = true;
-      }
-      {
-        name = "javascript";
-        shebangs = [ "node" ];
-        roots = [
-          "deno.json"
-          "deno.jsonc"
-          "package.json"
-          "tsconfig.json"
-        ];
-        file-types = [
-          "js"
-          "mjs"
-          "cjs"
-        ];
-        language-servers = [ "deno-lsp" ];
-        formatter = deno-fmt "js";
-        auto-format = true;
-      }
-      {
-        name = "json";
-        formatter = deno-fmt "json";
-        language-servers = [ "vscode-json" ];
-        auto-format = true;
-      }
-      {
-        name = "jsonc";
-        scope = "source.json";
-        injection-regex = "jsonc";
-        file-types = [
-          "jsonc"
-          { glob = "{deno,bun}.lock"; }
-        ];
-        formatter = deno-fmt "jsonc";
-        language-servers = [ "vscode-json" ];
-        auto-format = true;
-      }
-      {
-        name = "markdown";
-        formatter = deno-fmt "md";
-        language-servers = [ "marksman" ];
-        auto-format = true;
-      }
-      {
-        name = "css";
-        formatter = deno-fmt "css";
-        auto-format = true;
-      }
-      {
-        name = "c";
-        auto-format = true;
-      }
-      {
-        name = "cpp";
-        auto-format = true;
-      }
-      {
-        name = "python";
-        auto-format = true;
-      }
-    ];
+          auto-format = true;
+        }
+        {
+          name = "bash";
+          indent = {
+            tab-width = 4;
+            unit = "    ";
+          };
+          formatter = {
+            command = "shfmt";
+            args = [
+              "-i"
+              "4"
+            ];
+          };
+          auto-format = true;
+        }
+        {
+          name = "typst";
+          language-servers = [ "tinymist" ];
+          formatter = {
+            command = "typstyle";
+            args = [ "--wrap-text" ];
+          };
+          auto-format = true;
+        }
+        {
+          name = "toml";
+          formatter = {
+            command = "taplo";
+            args = [
+              "fmt"
+              "-"
+            ];
+          };
+          auto-format = true;
+        }
+        {
+          name = "html";
+          language-servers = [ "superhtml" ];
+          formatter = {
+            command = "superhtml";
+            args = [
+              "fmt"
+              "--stdin"
+            ];
+          };
+          auto-format = true;
+        }
+        {
+          name = "typescript";
+          shebangs = [ "deno" ];
+          roots = [
+            "deno.json"
+            "deno.jsonc"
+            "package.json"
+            "tsconfig.json"
+          ];
+          file-types = [
+            "ts"
+            "mts"
+            "cts"
+          ];
+          language-servers = [ "deno-lsp" ];
+          formatter = deno-fmt "ts";
+          auto-format = true;
+        }
+        {
+          name = "javascript";
+          shebangs = [ "node" ];
+          roots = [
+            "deno.json"
+            "deno.jsonc"
+            "package.json"
+            "tsconfig.json"
+          ];
+          file-types = [
+            "js"
+            "mjs"
+            "cjs"
+          ];
+          language-servers = [ "deno-lsp" ];
+          formatter = deno-fmt "js";
+          auto-format = true;
+        }
+        {
+          name = "json";
+          formatter = deno-fmt "json";
+          language-servers = [ "vscode-json" ];
+          auto-format = true;
+        }
+        {
+          name = "jsonc";
+          scope = "source.json";
+          injection-regex = "jsonc";
+          file-types = [
+            "jsonc"
+            { glob = "{deno,bun}.lock"; }
+          ];
+          formatter = deno-fmt "jsonc";
+          language-servers = [ "vscode-json" ];
+          auto-format = true;
+        }
+        {
+          name = "markdown";
+          formatter = deno-fmt "md";
+          language-servers = [ "marksman" ];
+          auto-format = true;
+        }
+        {
+          name = "css";
+          formatter = deno-fmt "css";
+          auto-format = true;
+        }
+        {
+          name = "c";
+          auto-format = true;
+        }
+        {
+          name = "cpp";
+          auto-format = true;
+        }
+        {
+          name = "python";
+          auto-format = true;
+        }
+        {
+          name = "kdl";
+          auto-format = true;
+        }
+      ];
   };
 
   # Configure desktop and utilities.
@@ -522,7 +527,10 @@ in
   # Configure bash.
   programs.bash = enable [ ] {
     historyFile = "$HOME/all/histfile";
-    historyControl = [ "ignoreboth" "erasedups" ];
+    historyControl = [
+      "ignoreboth"
+      "erasedups"
+    ];
     historyFileSize = 536870912;
     historySize = 1048576;
     sessionVariables = {
