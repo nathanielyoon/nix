2e2ed84930a060e2bf68db404afa29c60580924c
Nathaniel Yoon <nathanielyoon.expire025@slmails.com>
2025-12-07 12:30:10 -0500

    fix: update wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,19 @@
 local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
+-- Remove window padding.
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0
+}
+
+-- Configure tab bar.
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+
+-- Configure font.
 config.font = wezterm.font {
   family = "ZedMono Nerd Font",
   harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
